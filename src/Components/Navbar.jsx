3a74b075ca2edb7c277a7c3f2280c56847da53d5
Taiel98 <taielrodrigok@gmail.com>
2024-01-18 18:29:09 -0300

    primer paso, rutas

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,6 +8,10 @@ const Navbar = () => {
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to="./Routes/Home">Home</Link>
+      <Link to="./Routes/Detail">Detail</Link>
+      <Link to="./Routes/Favs">Favs</Link>
+      <Link to="./Routes/Contact">Contact</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Routes/Home';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import Contact from './Routes/Contact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}>
+          <Route path='/Routes/Home'element={<Home/>} />
+          <Route path='/Routes/Detail'element={<Detail/>} />
+          <Route path='/Routes/Favs/:id'element={<Favs/>} />
+          <Route path='/Routes/Contact'element={<Contact/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

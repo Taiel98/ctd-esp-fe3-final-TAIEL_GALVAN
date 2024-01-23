2b91fae2d1c5ad5792d96900d3761f0005b42b15
Taiel98 <taielrodrigok@gmail.com>
2024-01-23 17:39:02 -0300

    form contact

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [nombre, setNombre] = useState("");
+  const [mail, setMail] = useState("");
+
+  const onChangeNombre = (e) => setNombre(e.target.value);
+  const onChangeMail = (e) => setMail(e.target.value);
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    const esNombreValido = validarNombre(nombre)
+
+    if(!esNombreValido){
+      alert("Por favor verifique su información nuevamente");
+    }else{
+      alert(`Gracias ${nombre}, te contactaremos cuanto antes vía mail`)
+    }
+  }
+  
+  const validarNombre = (nombre) => {
+    const sinEspacios = nombre.trim();
+
+    if(sinEspacios.length > 2) {
+      return true;
+    }else{
+      return false;
+    }
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmitForm}>
+        <input 
+          type="text"
+          placeholder="Nombre"
+          value={nombre}
+          onChange={onChangeNombre}
+        />
+        <input 
+          type="email"
+          placeholder="mail"
+          value={mail}
+          onChange={onChangeMail}
+        />
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
